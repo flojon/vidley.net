04164dcdb3947829174ffe4427aff1d4d606edb6
Jonas Flodén <jonas@koalasoft.se>
2018-03-03 23:01:50 +0100

    Add authentication to startup

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using vidley.net.Data;
 
 namespace vidley.net
@@ -42,6 +45,23 @@ namespace vidley.net
                 };
             });
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => 
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = Configuration.GetSection("Jwt:Issuer").Value,
+                            ValidAudience = Configuration.GetSection("Jwt:Audience").Value,
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(Configuration.GetSection("Jwt:Key").Value)
+                            ),
+                        };
+                    });
+
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
         }
 
@@ -56,6 +76,8 @@ namespace vidley.net
             {
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseMvc();
